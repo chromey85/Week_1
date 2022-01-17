d078eaee9556fe66bd70aec839bf68d667e94bb3
chromey85 <yayyub85@gmail.com>
2022-01-17 12:20:23 +0000

    Initial commit of files

---
+++ Sandbox.js
@@ -0,0 +1,42 @@
+console.log();
+//console.log(`${header} Activity 7 - Prime Number checker`)
+console.log();
+
+// let n = 1;
+// while (n = 1); n < 20; {
+//     if (floor(n0 / n1) == n0 / n1) {
+//     console.log(`Yes, ${n} is a Prime Number`); n++;}
+//         else {console.log(`No, ${n} is not a Prime Number`)
+//         }n++;
+// };
+
+// while (n = 1); n < 20; n++; {
+//     if (n == (n / n) && n == (n / 1)) //!= (n / (Math.Round(Math.random() * 20)) != n)) 
+//     {console.log(`Yes, ${n} is a Prime Number`); n++;}
+//      else {console.log(`No, ${n} is not a Prime Number`)}
+    
+// };
+
+    //console.log(Number.isInteger(n / n && n / 1 || n / 21)) ? 'Yes, It is a Prime Number' : 'No, It is not a Prime Number'};
+//n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
+// for (let n = 1; n < 20; n++) {
+//         console.log(n)
+// };
+
+// let n = 1
+// while (n < 20) {
+//     if ((n == (n / n) && n == (n / 1)) || (n / (Math.random() * 20) != n)) {
+//     console.log(`Yes, ${n} is a Prime Number`);}
+//      else {(`No, ${n} is not a Prime Number`)}
+//         n++;
+// };
+
+let n = 1
+while (n < 20) {
+    if ((1 == (n / n) && (n == (n / 1))) || (n / (Math.random() * 20) != n)) {
+    console.log(`Yes, ${n} is a Prime Number`);}
+     else {(`No, ${n} is not a Prime Number`)}
+        n++;
+};
